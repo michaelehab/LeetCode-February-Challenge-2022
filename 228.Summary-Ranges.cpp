7da4f4e9c3bff828fb005db388a060ad65b20156
Michael Ehab <michaelehab16@gmail.com>
2022-02-28 16:22:42 +0200

    Added Day 28 Solution

---
+++ 228.Summary-Ranges.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums){
+        vector<string> ans;
+        if (!nums.size()) return ans;
+        
+        int start = nums[0], end = nums[0];
+        
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1] + 1) end = nums[i];
+            else {
+                if (start == end) ans.push_back(to_string(end));
+                else ans.push_back(to_string(start) + "->" + to_string(end)); 
+                
+                start = nums[i];
+                end = nums[i];
+            }
+        }
+        
+        if(start == end) ans.push_back(to_string(end));
+        else ans.push_back(to_string(start) + "->" + to_string(end));                 
+        
+        return ans;
+    }
+};
